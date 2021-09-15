11ad216c89570628b7c9cff015ad3cf9c039b3f6
James Kirk <jameskirk865@gmail.com>
2021-09-16 00:29:35 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 using System.IO;
+using System.Collections.Generic;
 
 namespace an631{
     class Program{
 		static bool err=false;
 		static string token,linkSend,linkAsk,linkUpd,key,id,path="c:/c#/an631/data/";//путь к директории с ботом
+		static StreamReader fr;
+		static StreamWriter fw;
 		
 		static string find(string json,string key,bool isint=false){//поиск значения по ключу в json записанным строкой
 			int a,b;
@@ -35,14 +39,51 @@ namespace an631{
 		}
 
 		static bool sendMsg(string peer,string text){
-			Console.Write("sm");
+			Console.Write("'");
 			string answ=request(linkSend+peer+"&message="+text);
 		return true;
 		}
 
+		static string toLowercase(string src){
+			string capt="QWERTYUIOPLKJHGFDSAZXCVBNM";
+			string low="qwertyuioplkjhgfdsazxcvbnm";
+			char[]r=src.ToCharArray();
+			for(int i=0;i<src.Length;++i)
+				if(capt.Contains(src[i]))
+					r[i]=low[capt.IndexOf(src[i])];
+			capt="ЁЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ";
+			low="ёйцукенгшщзхъэждлорпавыфячсмитьбю";
+			for(int i=0;i<src.Length;++i)
+				if(capt.Contains(src[i]))
+					r[i]=low[capt.IndexOf(src[i])];
+		return new String(r);
+		}
+
 		static void getKey(){
+			Console.Write("\nGETTING KEY:");
 			key=find(request(linkAsk),"key");
-			linkUpd="https://lp.vk.com/wh"+id+"?act=a_check&key="+key+"&wait=1&mode=2&ts=";
+			linkUpd="https://lp.vk.com/wh"+id+"?act=a_check&key="+key+"&wait=15&mode=2&ts=";
+			Console.WriteLine("COMPLETE");
+		}
+
+		static string read(string file){
+			fr=new StreamReader(file);
+			string data=fr.ReadLine();
+			fr.Close();
+		return data;
+		}
+		
+		static string read2end(string file){
+			fr=new StreamReader(file);
+			string data=fr.ReadToEnd();
+			fr.Close();
+		return data;
+		}
+
+		static void write(string file,string data){
+			fw=new StreamWriter(file);
+			fw.WriteLine(data);
+			fw.Close();
 		}
 
 		static bool check(int ts){
@@ -53,30 +94,20 @@ namespace an631{
 			if(answer.Contains("failed")){//допили потом 
 				Console.WriteLine("ERROR");
 				Console.Write(answer);
-				if(answer[10]=='2'){//непроверенный элемент
+				if(answer[10]=='2')//непроверенный элемент
 					getKey();
-					Console.WriteLine("попытка получить новый KEY");
-				}else{
+				else
 					err=true;
-				}
 				return false;
 			}
-			string msg=find(answer,"text");
-			if(msg.Contains("кис")){
-				sendMsg(find(answer,"peer_id",true),"мяу");
-			}
+			string msg=find(answer,"text"),user=find(answer,"from_id",true),peer=find(answer,"peer_id",true);
+			msg=toLowercase(msg);
+			if(msg.Contains("пинг"))
+				sendMsg(peer,"понг");
 		return true;
 		}
 		
-        static void Main(string[] args){
-			Console.WriteLine("startin");
-			StreamReader ft=new StreamReader(path+"login.txt");//данные о боте
-			id=ft.ReadLine();//в первой строке group_id
-			token=ft.ReadLine();//во второй токен с достаточными уровнями доступа
-		ft.Close();
-			linkSend="https://api.vk.com/method/messages.send?access_token="+token+"&v=5.131&random_id=0&peer_id=";
-			linkAsk="https://api.vk.com/method/groups.getLongPollServer?access_token="+token+"&group_id="+id+"&v=5.131";
-			getKey();
+		static void cycle(){
 			StreamReader fr=new StreamReader(path+"lasTS.txt");
 			int TS=int.Parse(fr.ReadLine());
 		fr.Close();
@@ -89,10 +120,31 @@ namespace an631{
 				}
 				Console.Write(".");
 				if(err){
-					Console.WriteLine("работа прекращена с ошибкой");
+					Console.WriteLine("\nUNKNOWN ERROR");
 				break;
 				}
 			}
+		}
+		
+        static void Main(string[] args){
+			Console.Write(DateTimeOffset.Now+"\nstartin");
+			StreamReader ft=new StreamReader(path+"login.txt");//данные о боте
+			id=ft.ReadLine();//в первой строке group_id
+			token=ft.ReadLine();//во второй токен с достаточными уровнями доступа
+		ft.Close();
+			linkSend="https://api.vk.com/method/messages.send?access_token="+token+"&v=5.131&random_id=0&peer_id=";
+			linkAsk="https://api.vk.com/method/groups.getLongPollServer?access_token="+token+"&group_id="+id+"&v=5.131";
+			getKey();
+			Thread core=new Thread(cycle);
+			core.Start();//поток с ботом
+			while(true){//основной поток проверяет ввод с клавиатуры. если нажата клавиша - завершение работы бота
+				ConsoleKeyInfo cki=Console.ReadKey();
+				if(cki.Key==ConsoleKey.Escape){
+					core.Interrupt();
+				break;
+				}
+			}
+			Console.WriteLine(DateTimeOffset.Now+"\nSHUTTING DOWN");
         }
     }
 }
